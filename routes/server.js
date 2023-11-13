fe0091637eaf6198a555045269f8bb7a1a0f7122
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2023-11-13 20:51:42 +0900

    server　postテストコード追加

--- routes/server.js
+++ routes/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 var router = express.Router();
 
-let messageJson = { message: "Hello Flutter!" };
+let messageJson = { message: "サーバー動いてるよ" };
 
 router.get("/", (req, res) => {
     res.send(messageJson);
+});
+
+router.post("/face", function (req, res) {
+    res.send({test : req.body});
 });
 
 module.exports = router;
