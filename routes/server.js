8da03dc048968c4d815a95828f4e08d713c0b2c5
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2023-11-17 02:01:03 +0900

    edit

--- routes/server.js
+++ routes/server.js
@@ -7,7 +7,7 @@ async function imageAnalys(imageData){
     const apiKey = process.env.VISION_API_KEY;
     const apiEndpoint = process.env.VISION_API_ENDPOINT;
     const visionApiUrl = `${apiEndpoint}?key=${apiKey}`
-    let responses;
+    let result;
     if (!apiKey) {
         console.log("Env 'VISION_API_KEY' must be set.");
         process.exit(1);
@@ -53,14 +53,13 @@ router.get("/", (req, res) => {
 });
 
 router.post("/face", function (req, res) {
-    console.log("success");
-    //const imageData = req.body.imageData; //画像のbase64変換した文字列をもらう
+    const imageData = req.body.imageData; //画像のbase64変換した文字列をもらう
     //const uid = req.query.uid;
 
     // Cloud Vision APIの処理かく
     // APIにbase64文字列を渡す&optionで表情分析を指定する
-    //imageAnalys(imageData);
-    res.send({"結果":req.body.imageData});
+    result = imageAnalys(imageData);
+    res.send({"結果":result});
 
     // uid使ってFirestoreのサブコレクション(体調データ)にbase64文字列を保存&感情情報!
 });
