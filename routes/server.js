de4db846e1cb94acbce8091e95e175fadf630646
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2023-11-16 15:24:35 +0900

    add test

--- routes/server.js
+++ routes/server.js
@@ -1,83 +1,67 @@
 const express = require("express");
 require('dotenv').config();
-console.log(require('dotenv').config());
 var router = express.Router();
+const axios = require('axios');
+
+async function imageAnalys(imageData){
+    const apiKey = process.env.VISION_API_KEY;
+    const apiEndpoint = process.env.VISION_API_ENDPOINT;
+    const visionApiUrl = `${apiEndpoint}?key=${apiKey}`
+    if (!apiKey) {
+        console.log("Env 'VISION_API_KEY' must be set.");
+        process.exit(1);
+    }
+
+    const options = {
+        requests: [
+          {
+            image: {
+              content: imageData,
+            },
+            features: [
+              {
+                type: "FACE_DETECTION",
+                maxResults: 1,
+              },
+            ],
+          },
+        ],
+    };
+
+    try {
+        const result = await axios.post(visionApiUrl, options);
+        console.log("Request success!");
+    
+        if (result.data && result.data.responses) {
+          const responses = result.data.responses;
+    
+          responses.forEach((response) => {
+            console.log("Face:");
+            response.landmarkAnnotations.forEach((annotation) =>
+              console.log(annotation)
+            );
+          });
+        }
+    } catch (error) {
+        console.error(error.response || error);
+    }
+    
+}
 
 router.get("/", (req, res) => {
     res.send({ message: "サーバー動いてるよ" });
-});
-
-router.post("/api_test", function (req, res) {
-    // const image = req.query.faceimage;
-    // const uid = req.query.uid;
-
-    //faceAPIの処理かく
-    //uid使ってFirestore参照&感情数値保存!
-    const appId = process.env.APP_ID
-    const firebaseApiKey = process.env.FIREBASE_API_KEY
-    const faceApiKey = process.env.FACE_API_KEY
-    
-    // FACE_API_KEY
-    res.send({
-    res: "Success!",
-    face_api_key: face_api_key,
-    });
+    imageAnalys();
 });
 
 router.post("/face", function (req, res) {
-    // const image = req.query.faceimage;
-    // const uid = req.query.uid;
+    const imageData = req.query.faceimage; //画像のbase64変換した文字列をもらう
+    //const uid = req.query.uid;
 
-    const axios = require('axios');
-    const fs = require('fs');
+    // Cloud Vision APIの処理かく
+    // APIにbase64文字列を渡す&optionで表情分析を指定する
+    imageAnalys(imageData);
 
-    // Face APIのエンドポイントとキーを設定します
-    const faceApiEndpoint = `https://${process.env.REGION}.api.cognitive.microsoft.com/face/v1.0/detect`;
-    const faceApiKey = process.env.FACE_API_KEY;
-
-    // 感情を推定したい画像のファイルパスを設定します
-    // その後､image変数を利用
-    const imagePath = 'IMG_4163.jpeg';
-
-    // 画像ファイルをバイナリデータとして読み込みます
-    const imageBuffer = fs.readFileSync(imagePath);
-
-    // Face APIに送信するヘッダーを設定します
-    const headers = {
-    'Content-Type': 'application/octet-json',
-    'Ocp-Apim-Subscription-Key': faceApiKey
-    };
-
-    // Face APIを呼び出します
-    axios({
-    method: 'post',
-    url: faceApiEndpoint,
-    data: imageBuffer,
-    headers: headers,
-    params: {
-        returnFaceId: 'true',
-        returnFaceLandmarks: 'false',
-        returnFaceAttributes: 'emotion',
-    },
-    })
-    .then(response => {
-    // Face APIから返された感情スコアを表示します
-    // const emotions = response.data[0].faceAttributes.emotion;
-    // console.log(emotions);
-        res.send({
-        res: "Success!",
-        emotions: response.data[0].faceAttributes.emotion,
-        http_code: response.status
-        });
-    })
-    .catch(error => {
-        res.send({
-        res: "Error!",
-        faceApiEndpoint:faceApiEndpoint,
-        faceApiKey: faceApiKey,
-        http_code: error.response.status
-        });
-    });
+    // uid使ってFirestoreのサブコレクション(体調データ)にbase64文字列を保存&感情情報!
 });
 
 module.exports = router;
