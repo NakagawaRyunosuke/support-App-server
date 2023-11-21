2454e5cafc1182c5869fa22ec8184c8b32ed2183
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2023-11-21 21:47:16 +0900

    edit

--- routes/server.js
+++ routes/server.js
@@ -3,19 +3,19 @@ const { initializeApp } = require('firebase-admin/app');
 require('dotenv').config();
 // Initialize Firebase
 
-// const firebaseServiceAccount = {
-//     "type": process.env.TYPE,
-//     "project_id": process.env.PROJECT_ID,
-//     "private_key_id": process.env.PRIVATE_KEY_ID,
-//     "private_key": process.env.PRIVATE_KEY,
-//     "client_email": process.env.CLIENT_EMAIL,
-//     "client_id": process.env.CLIENT_ID,
-//     "auth_uri": process.env.AUTH_URI,
-//     "token_uri": process.env.TOKEN_URI,
-//     "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_CERT_URL,
-//     "client_x509_cert_url": process.env.CLIENT_CERT_URL,
-//     "universe_domain": process.env.UNIVERSE_DOMAIN,
-// }
+const firebaseServiceAccount = {
+    "type": process.env.TYPE,
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY,
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_CERT_URL,
+    "client_x509_cert_url": process.env.CLIENT_CERT_URL,
+    "universe_domain": process.env.UNIVERSE_DOMAIN,
+}
 
 // const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 // const serviceAccount = firebaseServiceAccount;
