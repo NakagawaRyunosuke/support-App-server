0c241373e95c7ef95ecbb49abc16f83bb3b48d5a
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2023-11-22 19:42:28 +0900

    edit

--- routes/server.js
+++ routes/server.js
@@ -101,8 +101,8 @@ router.post("/diary", async function (req, res) {
   const voiceURL = "Users/<uid>/voice.m4a"; 
   const voiceText = "おはよう、今日は寒くて関節が痛いです。最近は綺麗な花を見つけたので散歩も楽しいです"; 
 
-  const emotionResult = await imageAnalys(imageURL); // APIにbase64文字列を渡し、結果をレスポンスで返却してます
-  setDiaryData(uid, emotionResult, imageURL, voiceURL, voiceText);  // uid使ってFirestoreのサブコレクション(Diary)に保存
+  //const emotionResult = await imageAnalys(imageURL); // APIに画像のURLを渡し、結果をレスポンスで返却してます
+  //setDiaryData(uid, emotionResult, imageURL, voiceURL, voiceText);  // uid使ってFirestoreのサブコレクション(Diary)に保存
   
   res.send("Success!");
 });
