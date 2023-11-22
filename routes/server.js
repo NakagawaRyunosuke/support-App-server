7c864a3d4c50261ae5daf180f4d063a6100ce63f
NakagawaRyunosuke <s2032112gn@s.chibakoudai.jp>
2023-11-22 19:35:47 +0900

    edit server.js

--- routes/server.js
+++ routes/server.js
@@ -1,8 +1,11 @@
 const admin = require('firebase-admin');
 const { initializeApp } = require('firebase-admin/app');
+const express = require("express");
 require('dotenv').config();
-// Initialize Firebase
+var router = express.Router();
+const axios = require('axios');
 
+// Initialize Firebase
 const firebaseServiceAccount = {
     "type": process.env.TYPE,
     "project_id": process.env.PROJECT_ID,
@@ -17,113 +20,91 @@ const firebaseServiceAccount = {
     "universe_domain": process.env.UNIVERSE_DOMAIN,
 }
 
-// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-const serviceAccount = firebaseServiceAccount;
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// const serviceAccount = firebaseServiceAccount;
 const app = initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 const db = admin.firestore();
 
-const express = require("express");
-require('dotenv').config();
-var router = express.Router();
-const axios = require('axios');
+//受け取った画像URLをもとに感情データ取得
+async function imageAnalys(imageURL){
+  const apiKey = process.env.VISION_API_KEY;
+  const apiEndpoint = process.env.VISION_API_ENDPOINT;
+  const visionApiUrl = `${apiEndpoint}?key=${apiKey}`
+  if (!apiKey) {
+      console.log("Env 'VISION_API_KEY' must be set.");
+      process.exit(1);
+  }
 
-async function imageAnalys(imageData){
-    const apiKey = process.env.VISION_API_KEY;
-    const apiEndpoint = process.env.VISION_API_ENDPOINT;
-    const visionApiUrl = `${apiEndpoint}?key=${apiKey}`
-    if (!apiKey) {
-        console.log("Env 'VISION_API_KEY' must be set.");
-        process.exit(1);
-    }
-
-    const options = {
-        requests: [
-          {
-            image: {
-              content: imageData,
-            },
-            features: [
-              {
-                type: "FACE_DETECTION",
-                maxResults: 1,
-              },
-            ],
+  const options = {
+      requests: [
+        {
+          image: {
+            source: {
+              imageUri: imageURL
+            }
           },
-        ],
+          features: [
+            {
+              type: "FACE_DETECTION",
+              maxResults: 1,
+            },
+          ],
+        },
+      ],
     };
 
     try {
-        const result = await axios.post(visionApiUrl, options);
-        console.log("Request success!");
-    
-        if (result.data && result.data.responses) {
-            const responses = result.data.responses;
-            return responses[0];
+      const result = await axios.post(visionApiUrl, options);
+      console.log("Request success!");
+  
+      if (result.data && result.data.responses) {
+        const responses = result.data.responses;
+        const emotionData = {
+          "joyLikelihood": responses[0]["faceAnnotations"][0]["joyLikelihood"],
+          "sorrowLikelihood": responses[0]["faceAnnotations"][0]["sorrowLikelihood"],
+          "angerLikelihood": responses[0]["faceAnnotations"][0]["angerLikelihood"],
+          "surpriseLikelihood": responses[0]["faceAnnotations"][0]["surpriseLikelihood"],
         }
+        return emotionData;
+      }
     } catch (error) {
-        console.error(error.response || error);
+      console.error(error.response || error);
     }
 }
 
-router.get("/", (req, res) => {
-    res.send({ message: "サーバー動いてるよ"});
-});
-
-router.post("/face", function (req, res) {
-    const imageData = req.body.imageData; //画像のbase64変換した文字列をもらう
-    //const uid = req.query.uid;
-
-    // Cloud Vision APIの処理かく
-    // APIにbase64文字列を渡し、結果をレスポンスで返却してます
-    imageAnalys(imageData).then((value) => {
-      const resData = JSON.stringify(value, null, 4);
-      res.send(`結果:${resData}`);
-    });
-
-    // uid使ってFirestoreのサブコレクション(体調データ)にbase64文字列を保存&感情情報!
-});
-
-async function setImageResult(uid,emotionResult){
+async function setDiaryData(uid, emotionResult, imageURL, voiceURL, voiceText){
   try{
-    const docRef = db.collection('Users').doc(uid).collection("Image");
-    const result = await docRef.add({
+    const docRef = db.collection('Users').doc(uid).collection("Diary");
+    await docRef.add({
       timestamp: admin.firestore.FieldValue.serverTimestamp(),
       emotionResult: emotionResult,
+      imageURL: imageURL,
+      voiceURL: voiceURL,
+      voiceText: voiceText
     });
   }catch(e){
     console.error('Error adding document: ', e);
   }
 }
 
-async function getImageUrl(uid){
-  try{
-    const docRef = db.collection('Users').doc(uid);
-    const doc = await docRef.get();
+router.get("/", (req, res) => {
+  res.send({ message: "サーバー動いてるよ"});
+});
 
-    if (doc.exists) {
-      const imageUrl = doc.data().imageUrl;
-      return imageUrl;
-    } else {
-      console.log('No such document!');
-      return null;
-    }
-  }catch(e){
-    console.error('Error getting document:', e);
-    return null;
-  }
-}
+router.post("/diary", async function (req, res) {
+  const imageURL = req.body.imageURL; //画像のURLもらう
+  //const voiceURL = req.body.voiceURL; //音声のURLもらう
+  //const voiceText = req.body.voiceText; //音声文字起こしテキストもらう
+  const uid = req.body.uid; //uidもらう
+  const voiceURL = "Users/<uid>/voice.m4a"; 
+  const voiceText = "おはよう、今日は寒くて関節が痛いです。最近は綺麗な花を見つけたので散歩も楽しいです"; 
 
-router.post("/test",async function(req,res){
-    const imageData = req.body.imageData;
-    const uid = req.body.uid;
-    // APIにbase64文字列を渡し、結果をレスポンスで返却してます
-    imageAnalys(imageData).then((value) => {
-      const resData = JSON.stringify(value, null, 4);
-      setImageResult(uid, resData);
-      res.send(`結果:${resData}`);
-    });
+  const emotionResult = await imageAnalys(imageURL); // APIにbase64文字列を渡し、結果をレスポンスで返却してます
+  setDiaryData(uid, emotionResult, imageURL, voiceURL, voiceText);  // uid使ってFirestoreのサブコレクション(Diary)に保存
+  
+  res.send("Success!");
 });
 
 module.exports = router;
